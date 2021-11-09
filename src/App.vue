e659085a70920a54ff4400bbb4cdf3c4bb6b55fe
arel18 <aurellio.fishandy@ui.com>
2021-11-09 14:56:37 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <Approval/>
+</template>
+
+<script>
+import Approval from './components/Approval.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Approval
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  width: 360px;
+  height: 640px;
+}
+</style>
